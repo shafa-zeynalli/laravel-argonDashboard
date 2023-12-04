c7dd101014c4ac8bf768fe0a6710e3a03993833d
shafa-zeynalli <shafa.zeynalli@outlook.com>
2023-12-04 20:37:25 +0400

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+//        if (Auth::check()){
+//            dd('yes');
+//        }
+//        Auth::logout();
+
+       return view('admin.dashboard');
+    }
+
+    public function tables()
+    {
+        $users = User::query()->get();
+        $products = Product::query()->get();
+//        dd($products);
+       return view('admin.table.user_table', compact(['users', 'products']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function login(){
+
+//        if (!Auth::check()){
+            return view('admin.auth.login');
+//        }else{
+//            redirect();
+//        }
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+//            Auth::login($credentials);
+            return redirect()->intended('admin');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function users(){
+        $users = User::query()->paginate(10);
+
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function products(){
+        $products = Product::query()->with('user')->paginate(10);
+//        dd($products);
+        return view('admin.products.index', compact('products'));
+    }
+
+
+    public function filterUsers(Request $request)
+    {
+        // Formdan gelen verileri al
+        $name = $request->input('name');
+        $email = $request->input('email');
+
+        // Query oluştur
+        $userQuery = User::query();
+
+        // Filtreleme koşullarını ekle
+        if ($name) {
+            $userQuery->where('name', 'like', "%$name%");
+        }
+
+        if ($email) {
+            $userQuery->where('email', 'like', "%$email%");
+        }
+
+        // Sonuçları getir
+        $users = $userQuery->paginate(10);
+
+//        dd($users);
+        // View'e verileri gönder
+        return view('admin.users.index', compact('users'));
+    }
+
+}
---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.admin')
+
+@section('breadcrumb')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="{{route('admin.products')}}">Pages</a></li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Products</li>
+        </ol>
+        <h6 class="font-weight-bolder text-white mb-0">Products</h6>
+    </nav>
+@endsection
+
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header pb-0">
+                    <h6>Products list</h6>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                            <tr>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Id
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    User
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    User Name
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                    Slug
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                    Price
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                    Status
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Created At
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($products as $product)
+                                <tr>
+                                    <td>
+                                        <p>{{ $product->id }}</p>
+                                    </td>
+                                    <td>
+                                        <p>{{ $product->user->name }}</p>
+                                    </td>
+                                    <td class="">
+                                        <p>{{ $product->name }}</p>
+                                    </td>
+                                    <td class="">
+                                        <p>{{ $product->slug }}</p>
+                                    </td>
+                                    <td class="">
+                                        <p>{{ $product->price }}</p>
+                                    </td>
+                                    <td class="">
+                                        <p>{{ $product->status }}</p>
+                                    </td>
+                                    <td class="">
+                                        <p>{{ $product->created_at?$product->created_at->format('Y/m/d'):'' }}</p>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div>
+                            {{ $products->links('pagination::bootstrap-5') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
